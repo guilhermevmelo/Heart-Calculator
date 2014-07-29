d94594a1799d99e1c4b12a2065d20e7a85c2ca52
guilhermevmelo <guilhermevmelo@gmail.com>
2014-07-29 17:19:03 +0100

    Modified the units so all percentages are now passed as floating points between 0 and 1.
    Added the dashed lines concerning medication.

--- scripts/main.js
+++ scripts/main.js
@@ -46,8 +46,8 @@ function Graph(definitions) {
 		 * as its css properties specifies
 		 */
 		
-		this.drawable.w = parseInt((this.limit.x - this.origin.x)/100 * this.w);
-		this.drawable.h = parseInt((this.limit.y - (this.axisOrigin.y*100 + this.origin.y))/100 * this.h);
+		this.drawable.w = parseInt((this.limit.x - this.origin.x) * this.w);
+		this.drawable.h = parseInt((this.limit.y - (this.axisOrigin.y + this.origin.y)) * this.h);
 		
 		//Debug: console.log("this.drawable.w", this.drawable.w, "this.drawable.h", this.drawable.h);
 		
@@ -61,51 +61,76 @@ function Graph(definitions) {
 		/*The rectangles must be drawn first so the axis overlap its borders */
 		/* The green */
 		context.fillStyle = "#36D936";
-		context.fillRect(this.axisOrigin.x * this.w, this.axisOrigin.y * this.h, (((this.limit.x+100)/200)-this.axisOrigin.x - 0.0125) * this.w, 0.2 * this.drawable.h);
+		context.fillRect(this.axisOrigin.x * this.w, (this.axisOrigin.y + this.origin.y) * this.h, (((this.limit.x+1)/2)-this.axisOrigin.x - 0.0125) * this.w, 0.2 * this.drawable.h);
 		
 		/* The yellow */
 		context.fillStyle = "#FFFF3F";
-		context.fillRect(this.axisOrigin.x * this.w, this.axisOrigin.y * this.h + 0.2 * this.drawable.h, (((this.limit.x+100)/200)-this.axisOrigin.x - 0.0125) * this.w, 0.1 * this.drawable.h);
+		context.fillRect(this.axisOrigin.x * this.w, (this.axisOrigin.y + this.origin.y) * this.h + 0.2 * this.drawable.h, (((this.limit.x+1)/2)-this.axisOrigin.x - 0.0125) * this.w, 0.1 * this.drawable.h);
 		
 		/* The orange */
 		context.fillStyle = "#FFB23F";
-		context.fillRect(this.axisOrigin.x * this.w, this.axisOrigin.y * this.h + 0.3 * this.drawable.h, (((this.limit.x+100)/200)-this.axisOrigin.x - 0.0125) * this.w, 0.1 * this.drawable.h);
+		context.fillRect(this.axisOrigin.x * this.w, (this.axisOrigin.y + this.origin.y) * this.h + 0.3 * this.drawable.h, (((this.limit.x+1)/2)-this.axisOrigin.x - 0.0125) * this.w, 0.1 * this.drawable.h);
 		
 		/* The red */
 		context.fillStyle = "#FF3F3F";
-		context.fillRect(this.axisOrigin.x * this.w, this.axisOrigin.y * this.h + 0.4 * this.drawable.h, (((this.limit.x+100)/200)-this.axisOrigin.x - 0.0125) * this.w, 0.6 * this.drawable.h);
+		context.fillRect(this.axisOrigin.x * this.w, (this.axisOrigin.y + this.origin.y) * this.h + 0.4 * this.drawable.h, (((this.limit.x+1)/2)-this.axisOrigin.x - 0.0125) * this.w, 0.6 * this.drawable.h);
 		
 		/* Now the nice white-transparent background for the drawing area */
 		context.fillStyle = "rgba(255, 255, 255, 0.7)";
-		context.fillRect(this.origin.x/100 * this.w, (this.axisOrigin.y + this.origin.y) * this.h, this.drawable.w, this.drawable.h);
+		context.fillRect(this.origin.x * this.w, (this.axisOrigin.y + this.origin.y) * this.h, this.drawable.w, this.drawable.h);
 		
-		//Debug: console.log(this.axisOrigin.x, this.origin.x/100 * this.w);
+		//Debug: console.log(this.axisOrigin.x, this.origin.x * this.w);
 		
 		/* Now that the background has been drawn, lets draw the axes */
 		context.beginPath();
 		/* y-axis */
 		context.moveTo(this.axisOrigin.x * this.w, this.axisOrigin.y * this.h);
-		context.lineTo(this.axisOrigin.x * this.w, (this.limit.y + 100)/200 * this.h);
-		context.lineTo(this.axisOrigin.x * this.w - 5, (this.limit.y + 100)/200 * this.h - 10);
-		context.moveTo(this.axisOrigin.x * this.w + 5, (this.limit.y + 100)/200 * this.h - 10);
-		context.lineTo(this.axisOrigin.x * this.w, (this.limit.y + 100)/200 * this.h);
+		context.lineTo(this.axisOrigin.x * this.w, (this.limit.y + 1)/2 * this.h);
+		context.lineTo(this.axisOrigin.x * this.w - 5, (this.limit.y + 1)/2 * this.h - 10);
+		context.moveTo(this.axisOrigin.x * this.w + 5, (this.limit.y + 1)/2 * this.h - 10);
+		context.lineTo(this.axisOrigin.x * this.w, (this.limit.y + 1)/2 * this.h);
 		
 		/* x-axis */
 		context.moveTo(this.axisOrigin.x * this.w, this.axisOrigin.y * this.h);
-		context.lineTo((this.limit.x + 100)/200 * this.w, this.axisOrigin.y * this.h);
-		context.lineTo((this.limit.x + 100)/200 * this.w - 10, this.axisOrigin.y * this.h + 5);
-		context.moveTo((this.limit.x + 100)/200 * this.w - 10, this.axisOrigin.y * this.h - 5);
-		context.lineTo((this.limit.x + 100)/200 * this.w, this.axisOrigin.y * this.h);
+		context.lineTo((this.limit.x + 1)/2 * this.w, this.axisOrigin.y * this.h);
+		context.lineTo((this.limit.x + 1)/2 * this.w - 10, this.axisOrigin.y * this.h + 5);
+		context.moveTo((this.limit.x + 1)/2 * this.w - 10, this.axisOrigin.y * this.h - 5);
+		context.lineTo((this.limit.x + 1)/2 * this.w, this.axisOrigin.y * this.h);
 		
 		context.strokeStyle = "#000000";
 		context.lineWidth = 2;
-		context.lineCap = "square";
+		context.lineCap = "squared";
+		context.lineJoin = "round";
 		context.stroke();
-		
 		context.closePath();
 		
+		/* dashed lines */
+		context.beginPath();
+		context.moveTo(this.axisOrigin.x * this.w, (this.axisOrigin.y + this.origin.y) * this.h + 0.2 * this.drawable.h);
+		context.lineTo((this.limit.x + 1)/2 * this.w, (this.axisOrigin.y + this.origin.y) * this.h + 0.2 * this.drawable.h);
+		context.moveTo(this.axisOrigin.x * this.w, (this.axisOrigin.y + this.origin.y) * this.h + 0.4 * this.drawable.h);
+		context.lineTo((this.limit.x + 1)/2 * this.w, (this.axisOrigin.y + this.origin.y) * this.h + 0.4 * this.drawable.h);
+		context.setLineDash([8, 10]);
+		context.strokeStyle = "#333";
+		context.lineWidth = 1;
+		context.lineCap = "square";
+		context.stroke();
+		context.closePath();
 		
 		context.restore();
+		
+		/* Now we follow with the texts */
+		var aux = 0.0337 * this.h;
+		context.font = aux + 'px "Shadows Into Light", sans-serif';
+		context.fillStyle = "#000";
+		context.fillText("Very High", (this.axisOrigin.x + 0.02)*this.w, 0.35*this.h);
+		context.fillText("50%", (this.axisOrigin.x + 0.845)*this.w, 0.09*this.h);
+		context.fillText("High", (this.axisOrigin.x + 0.02)*this.w, 0.65*this.h);
+		context.fillText("20%", (this.axisOrigin.x + 0.845)*this.w, 0.63*this.h);
+		context.fillText("Medium", (this.axisOrigin.x + 0.02)*this.w, 0.74*this.h);
+		context.fillText("15%", (this.axisOrigin.x + 0.85)*this.w, 0.72*this.h);
+		context.fillText("Low", (this.axisOrigin.x + 0.02)*this.w, 0.87*this.h);
+		context.fillText("10%", (this.axisOrigin.x + 0.85)*this.w, 0.81*this.h);
 	};
 	
 	this.initialize();
@@ -124,9 +149,9 @@ function Graph(definitions) {
 		
 		context.save();
 		
-		context.translate(this.origin.x/100 * this.w, this.h * (1 - (this.axisOrigin.y + this.origin.y)));
+		context.translate(this.origin.x * this.w, this.h * (1 - (this.axisOrigin.y + this.origin.y)));
 		
-		//Debug: console.log(this.origin.x/100 * this.w, this.h * (1 - this.origin.y/100));
+		//Debug: console.log(this.origin.x * this.w, this.h * (1 - this.origin.y));
 		
 		context.scale(1, -1);
 		context.beginPath();
@@ -142,7 +167,7 @@ function Graph(definitions) {
 		context.shadowBlur = 2;
 		
 		for (var i = settings.from * this.xDotDist; i <= settings.to * this.xDotDist; i++/* += this.xDotDist*/){
-			//if (settings.f(i / this.xDotDist) * this.yDotDist <= this.limit.y/100*this.drawable.h) 
+			//if (settings.f(i / this.xDotDist) * this.yDotDist <= this.limit.y*this.drawable.h) 
 				context.lineTo(i, settings.f(i / this.xDotDist) * this.yDotDist);
 		}
 		
@@ -171,14 +196,14 @@ function validate() {
 		$("#Step1 p.nextStepLinkContainer").fadeIn('slow');
 }
 
-$(document).ready(function() {
+$(window).load(function() {
 	var graph = new Graph({
 		canvasId: 	"ResultCanvas",
 		ratioWH: 	1.5,
 		resolution: {x: 95, y: 90},
 		axisOrigin: {x: 0.03, y: 0.05},
-		origin: 	{x: 14.80, y: 0},
-		limit: 		{x: 86.00, y: 95}
+		origin: 	{x: 0.1480, y: 0.0},
+		limit: 		{x: 0.8600, y: 0.95}
 	});
 	
 	$(window).resize(function() {
